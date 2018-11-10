6c2a64ae4d79c537e804b676cf3b38d67ab8e87f
Ivan Franco <ifranco@tupaca.com>
2018-11-10 13:29:03 -0300

    First two TestDome exercices

---
+++ Inherite Entities/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class TextInput
+{
+    private string input = string.Empty;
+
+    public void Add(char c)
+    {
+        if (isValueValid(c))
+            input += c.ToString();
+    }
+
+    protected virtual bool isValueValid(char c)
+    {
+        return char.IsLetterOrDigit(c);
+    }
+
+    public string GetValue()
+    {
+        return input;
+    }
+}
+
+public class NumericInput : TextInput
+{
+
+    protected override bool isValueValid(char c)
+    {
+        return char.IsDigit(c);
+    }
+}
+
+public class UserInput
+{
+    public static void Main(string[] args)
+    {
+        TextInput input = new NumericInput();
+        input.Add('1');
+        input.Add('a');
+        input.Add('0');
+        Console.WriteLine(input.GetValue());
+    }
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Palindrome
+{
+    public static bool IsPalindrome(string word)
+    {
+        if (word.Length > 1)
+        {
+            string wordToEvaluate = word.ToLower();
+
+            int initialValue = 0;
+            for (int i = word.Length - 1; i > -1; i--)
+            {
+                if (wordToEvaluate.Substring(initialValue, 1) != wordToEvaluate.Substring(i, 1))
+                    return false;
+                initialValue++;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Palindrome.IsPalindrome("Deleveled"));
+    }
+}
